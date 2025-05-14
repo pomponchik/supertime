8b2c24e2b09825300394037b613829c2a0e638c5
esblinov <esblinov@MSK-JLG3YP9XJG.local>
2025-05-14 14:46:31 +0300

    new tests

--- tests/test_function.py
+++ tests/test_function.py
@@ -6,6 +6,17 @@ from emptylog import MemoryLogger
 from supertime import supersleep
 
 
+global_sleep_time = 0.001
+
+before = time()
+supersleep(global_sleep_time)
+real_global_sync_sleep_time = time() - before
+
+before = time()
+run(supersleep(global_sleep_time))
+real_global_async_sleep_time = time() - before
+
+
 def test_simple_sync_sleep():
     sleep_time = 0.001
 
@@ -50,3 +61,11 @@ def test_logging_when_async_sleep():
         f'Run async sleep {sleep_time} sec...',
         'The end of async sleeping.',
     ]
+
+
+def test_global_sync_sleep():
+    assert real_global_sync_sleep_time >= global_sleep_time
+
+
+def test_global_async_sleep():
+    assert real_global_async_sleep_time >= global_sleep_time
