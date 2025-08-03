f8492f038e8d6c79f785863587221f2eb834f85c
esblinov <esblinov@avito.ru>
2025-08-03 23:22:39 +0300

    pragma comments

--- supertime/function.py
+++ supertime/function.py
@@ -7,8 +7,8 @@ from transfunctions import superfunction, sync_context, async_context, await_it
 
 @superfunction(tilde_syntax=False)
 def supersleep(number):
-    with sync_context:
+    with sync_context:  # pragma: no cover
         sync_sleep(number)
 
-    with async_context:
+    with async_context:  # pragma: no cover
         await_it(async_sleep(number))
