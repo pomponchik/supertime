fb243ba823c66e6496d7dc2234073c7bfe1ff46a
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2025-10-30 14:45:15 +0300

    Add type ignore annotation to supersleep

--- supertime/function.py
+++ supertime/function.py
@@ -6,7 +6,7 @@ from transfunctions import superfunction, sync_context, async_context, await_it
 
 
 @superfunction(tilde_syntax=False)
-def supersleep(number: Union[int, float]) -> NoReturn:
+def supersleep(number: Union[int, float]) -> NoReturn:  # type: ignore[misc]
     with sync_context:  # pragma: no cover
         sync_sleep(number)
     with async_context:  # pragma: no cover
